526aafca046121a8de52bdf4922fbf6830550fba
Bailey <bailey.jannuzzi@gmail.com>
2026-01-27 18:54:12 -0700

    more updates

--- scripts/README.md
+++ scripts/README.md
@@ -1,3 +1,3 @@
 | Script | Description |
 | --- | --- |
-| **script-01_quality_control.sh** | QC and trimming pipeline. Output in the _data/qc/_ directory.
+| **script-01_quality_control.sh** | QC and trimming pipeline. Output in the _data/qc/_ directory. Details on command parameters/flags are described at the end of each script file.
--- scripts/script-01_quality_control.sh
+++ scripts/script-01_quality_control.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+######################################
+
+# Script for running quality control and trimming reads.
+# Description and reasoning for each flag command are included at the end of this file.
+
+######################################
+
 # Changing directory to make sure script runs in the correct folder.
 cd ../data/raw/compressed_files
 
@@ -40,3 +47,29 @@ fi
 echo ""
 
 done
+
+
+######################################
+
+# Here is a description of each of the flags used in the fastp command. These were chosen based on standard parameters for handling Illumina PE reads.
+# Some flag descriptions are taken from the fastp -h description file.
+
+# --in1 ~ Read1 input file name
+# --in2 ~ Read2 input file name
+# --out1 ~ Read1 output file name
+# --out2 ~ Read2 output file name
+# --html ~ Output html file name
+# --json ~ Output json file name
+# --detect_adapter_for_pe ~ Takes a bit longer than other options, but this flag detects PE data to get more clean data.
+# --trim_front1 20 ~ Trimming 20 bases infront of read1, best for trimming adaptors from Illumina
+# --trim_tail1 20 ~ Trimming 20 bases after read1, best for trimming adaptors from Illumina
+# --trim_front2 20 ~ Trimming 20 bases infront of read2, best for trimming adaptors from Illumina
+# --trim_tail2 20 ~ Trimming 20 bases after read2, best for trimming adaptors from Illumina
+# --cut_front ~ Move a sliding window from front \(5'\) to tail, drop the bases in the window if its mean quality < threshold, stop otherwise.
+# --cut_tail ~ Move a sliding window from tail \(3'\) to front, drop the bases in the window if its mean quality < threshold, stop otherwise.
+# --length_required 100 ~ Discard reads shorter than 100bp.
+# --thread 12 ~ Increasing thread number to run faster.
+# --qualified_quality_phred 20 ~ Using a Q20 for quality checking, though a Q20-Q30 tends to be standard. Fastp report will detail both a Q20 and Q30 percentage.
+# --correction ~ Enables base correction in overlapped regions (only for PE data).
+
+######################################
